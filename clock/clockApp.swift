a10ccc1b5b845e21d97ef0a004579a1726b0b1e0
{rinion1590} <rinion1590@gmail.com>
2023-07-01 00:40:05 +0900

    Initial Commit

---
+++ clock/clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockApp.swift
+//  clock
+//
+//  Created by Nagasaki Ryu on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
